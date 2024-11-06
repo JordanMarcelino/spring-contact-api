9f40d3ae69d1669f5ad67877ba3c992c8078a991
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-11-06 15:49:30 +0700

    feat: add bcrypt and message constant

---
+++ src/main/java/jordanmarcelino/contact/util/Message.java
@@ -0,0 +1,9 @@
+package jordanmarcelino.contact.util;
+
+public class Message {
+
+    public static final String SUCCESS = "success";
+    public static final String BAD_REQUEST = "bad request";
+    public static final String INTERNAL_SERVER_ERROR = "internal server error";
+
+}
