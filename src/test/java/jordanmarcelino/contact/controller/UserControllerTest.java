842a1a8a58e8aaddfc98eef137c547eb48d42019
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-11-06 23:30:00 +0700

    test: add user update profile test

--- src/test/java/jordanmarcelino/contact/controller/UserControllerTest.java
+++ src/test/java/jordanmarcelino/contact/controller/UserControllerTest.java
@@ -3,12 +3,14 @@ package jordanmarcelino.contact.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
-import jordanmarcelino.contact.dto.Token;
+import jakarta.validation.ConstraintViolationException;
+import jordanmarcelino.contact.dto.UpdateUserRequest;
 import jordanmarcelino.contact.dto.UserResponse;
 import jordanmarcelino.contact.dto.WebResponse;
 import jordanmarcelino.contact.entity.User;
 import jordanmarcelino.contact.repository.UserRepository;
 import jordanmarcelino.contact.service.UserService;
+import jordanmarcelino.contact.util.Message;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +27,9 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.MockMvcBuilder.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -135,5 +136,115 @@ class UserControllerTest {
 
         verify(userRepository, times(1)).findByToken(anyString());
         verify(userService, times(1)).get(any(User.class));
+    }
+
+    @Test
+    void testUpdateProfileUnauthorized() throws Exception {
+        when(userService.update(any(User.class), any(UpdateUserRequest.class)))
+                .thenReturn(new UserResponse());
+
+        UpdateUserRequest request = new UpdateUserRequest();
+        request.setName("test");
+        request.setPassword("supersecret");
+        mockMvc.perform(
+                put("/api/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isUnauthorized()
+        ).andDo(result -> {
+            WebResponse<UserResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {
+                    }
+            );
+
+            assertNull(response.getData());
+            assertNull(response.getErrors());
+        });
+
+        verify(userService, times(0)).update(any(User.class), any(UpdateUserRequest.class));
+    }
+
+    @Test
+    void testUpdateProfileBadRequest() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("test");
+        user.setName("test");
+        user.setTokenExpiredAt(Instant.now().plus(Duration.ofDays(1L)).toEpochMilli());
+
+        when(userRepository.findByToken(anyString()))
+                .thenReturn(Optional.of(user));
+        when(userService.update(any(User.class), any(UpdateUserRequest.class)))
+                .thenThrow(ConstraintViolationException.class);
+
+        UpdateUserRequest request = new UpdateUserRequest();
+        request.setName("");
+        request.setPassword("");
+        mockMvc.perform(
+                put("/api/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<UserResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {
+                    }
+            );
+
+            assertEquals(Message.BAD_REQUEST, response.getMessage());
+            assertNull(response.getData());
+            assertNotNull(response.getErrors());
+        });
+
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(userService, times(1)).update(any(User.class), any(UpdateUserRequest.class));
+    }
+
+    @Test
+    void testUpdateProfileSuccess() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("test");
+        user.setName("test");
+        user.setTokenExpiredAt(Instant.now().plus(Duration.ofDays(1L)).toEpochMilli());
+
+        UserResponse wantRes = new UserResponse(1L, "test", "new test");
+        when(userRepository.findByToken(anyString()))
+                .thenReturn(Optional.of(user));
+        when(userService.update(any(User.class), any(UpdateUserRequest.class)))
+                .thenReturn(wantRes);
+
+        UpdateUserRequest request = new UpdateUserRequest();
+        request.setName("new test");
+        request.setPassword("newsupersecret");
+        mockMvc.perform(
+                put("/api/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+            WebResponse<UserResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {
+                    }
+            );
+
+            assertEquals(Message.SUCCESS, response.getMessage());
+            assertEquals(wantRes, response.getData());
+            assertNull(response.getErrors());
+        });
+
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(userService, times(1)).update(any(User.class), any(UpdateUserRequest.class));
     }
 }
