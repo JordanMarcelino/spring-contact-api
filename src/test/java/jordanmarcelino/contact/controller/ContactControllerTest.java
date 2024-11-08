54e7fd06af5137a9325d9ff3896acd666e1d15d7
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-11-08 22:26:32 +0700

    test: add update and delete contact test

--- src/test/java/jordanmarcelino/contact/controller/ContactControllerTest.java
+++ src/test/java/jordanmarcelino/contact/controller/ContactControllerTest.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import jakarta.validation.ConstraintViolationException;
-import jordanmarcelino.contact.dto.ContactResponse;
-import jordanmarcelino.contact.dto.CreateContactRequest;
-import jordanmarcelino.contact.dto.GetContactRequest;
-import jordanmarcelino.contact.dto.WebResponse;
+import jordanmarcelino.contact.dto.*;
 import jordanmarcelino.contact.entity.User;
 import jordanmarcelino.contact.exception.NotFoundException;
 import jordanmarcelino.contact.repository.UserRepository;
@@ -30,8 +27,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -119,6 +115,38 @@ class ContactControllerTest {
     }
 
     @Test
+    void testCreateContactSuccess() throws Exception {
+        CreateContactRequest request = new CreateContactRequest();
+        request.setFirstName("test");
+        request.setLastName("test");
+        request.setEmail("test@gmail.com");
+        request.setPhone("0123456789");
+        ContactResponse wantRes = new ContactResponse(1L, request.getFirstName(), request.getLastName(), request.getEmail(), request.getPhone());
+        when(contactService.save(any(CreateContactRequest.class)))
+                .thenReturn(wantRes);
+        mockMvc.perform(
+                post("/api/contacts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isCreated()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+            assertEquals(Message.SUCCESS, response.getMessage());
+            assertEquals(wantRes, response.getData());
+            assertNull(response.getErrors());
+        });
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(contactService, times(1)).save(any(CreateContactRequest.class));
+    }
+
+    @Test
     void testGetContactUnauthorized() throws Exception {
         mockMvc.perform(
                 get("/api/contacts/1")
@@ -197,4 +225,210 @@ class ContactControllerTest {
         verify(contactService, times(1)).get(any(GetContactRequest.class));
     }
 
+    @Test
+    void testUpdateContactUnauthorized() throws Exception {
+        UpdateContactRequest request = new UpdateContactRequest();
+        request.setFirstName("test");
+        request.setLastName("test");
+        request.setEmail("test@gmail.com");
+        request.setPhone("0123456789");
+
+        mockMvc.perform(
+                put("/api/contacts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isUnauthorized()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+
+            assertNull(response.getData());
+            assertNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void testUpdateContactBadRequest() throws Exception {
+        when(contactService.update(any(UpdateContactRequest.class)))
+                .thenThrow(ConstraintViolationException.class);
+
+        UpdateContactRequest request = new UpdateContactRequest();
+        request.setFirstName("");
+        request.setLastName("");
+        request.setEmail("");
+        request.setPhone("");
+
+        mockMvc.perform(
+                put("/api/contacts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+
+            assertNull(response.getData());
+            assertNotNull(response.getErrors());
+        });
+
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(contactService, times(1)).update(any(UpdateContactRequest.class));
+    }
+
+    @Test
+    void testUpdateContactNotFound() throws Exception {
+        when(contactService.update(any(UpdateContactRequest.class)))
+                .thenThrow(new NotFoundException());
+
+        UpdateContactRequest request = new UpdateContactRequest();
+        request.setFirstName("test");
+        request.setLastName("test");
+        request.setEmail("test@gmail.com");
+        request.setPhone("0123456789");
+
+        mockMvc.perform(
+                put("/api/contacts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isNotFound()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+
+            assertNull(response.getData());
+            assertNull(response.getErrors());
+        });
+
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(contactService, times(1)).update(any(UpdateContactRequest.class));
+    }
+
+    @Test
+    void testUpdateContactSuccess() throws Exception {
+        UpdateContactRequest request = new UpdateContactRequest();
+        request.setFirstName("test");
+        request.setLastName("test");
+        request.setEmail("test@gmail.com");
+        request.setPhone("0123456789");
+
+        ContactResponse wantRes = new ContactResponse();
+        wantRes.setId(1L);
+        wantRes.setFirstName(request.getFirstName());
+        wantRes.setLastName(request.getLastName());
+        wantRes.setEmail(request.getEmail());
+        wantRes.setPhone(request.getPhone());
+
+        when(contactService.update(any(UpdateContactRequest.class)))
+                .thenReturn(wantRes);
+
+        mockMvc.perform(
+                put("/api/contacts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+
+            assertEquals(Message.SUCCESS, response.getMessage());
+            assertEquals(wantRes, response.getData());
+            assertNull(response.getErrors());
+        });
+
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(contactService, times(1)).update(any(UpdateContactRequest.class));
+    }
+
+    @Test
+    void testDeleteContactUnauthorized() throws Exception {
+        mockMvc.perform(
+                delete("/api/contacts/1")
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                status().isUnauthorized()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+
+            assertNull(response.getData());
+            assertNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void testDeleteContactNotFound() throws Exception {
+        doThrow(new NotFoundException()).when(contactService).delete(any(DeleteContactRequest.class));
+
+        mockMvc.perform(
+                delete("/api/contacts/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+        ).andExpectAll(
+                status().isNotFound()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+
+            assertNull(response.getData());
+            assertNull(response.getErrors());
+        });
+
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(contactService, times(1)).delete(any(DeleteContactRequest.class));
+    }
+
+    @Test
+    void testDeleteContactSuccess() throws Exception {
+        doNothing().when(contactService).delete(any(DeleteContactRequest.class));
+
+        mockMvc.perform(
+                delete("/api/contacts/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .cookie(apiKey)
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+            WebResponse<ContactResponse> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<WebResponse<ContactResponse>>() {
+                    }
+            );
+
+            assertEquals(Message.SUCCESS, response.getMessage());
+            assertNull(response.getData());
+            assertNull(response.getErrors());
+        });
+
+        verify(userRepository, times(1)).findByToken(anyString());
+        verify(contactService, times(1)).delete(any(DeleteContactRequest.class));
+    }
 }
