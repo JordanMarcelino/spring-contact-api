9350094ed108da2f85eacd7c94b8d34ac5f4722b
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-11-09 15:05:08 +0700

    chore: add db migration and properties

--- src/main/resources/migrations/schema.sql
+++ src/main/resources/migrations/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users
     username         VARCHAR(100) UNIQUE NOT NULL,
     name             VARCHAR(100)        NOT NULL,
     hash_password    VARCHAR(255)        NOT NULL,
-    token            VARCHAR(255) UNIQUE NOT NULL,
+    token            VARCHAR(255) UNIQUE,
     token_expired_at BIGINT,
     created_at       TIMESTAMP           NOT NULL,
     updated_at       TIMESTAMP           NOT NULL
@@ -18,10 +18,10 @@ CREATE TABLE IF NOT EXISTS contacts
 (
     id         BIGSERIAL PRIMARY KEY,
     user_id    BIGINT REFERENCES users (id) NOT NULL,
-    first_name VARCHAR(255)                 NOT NULL,
-    last_name  VARCHAR(255),
+    first_name VARCHAR(100)                 NOT NULL,
+    last_name  VARCHAR(100),
     email      VARCHAR(255),
-    phone      VARCHAR(255),
+    phone      VARCHAR(100),
     created_at TIMESTAMP                    NOT NULL,
     updated_at TIMESTAMP                    NOT NULL
 );
